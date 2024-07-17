49023a1995f061f4b827b74a45918ed30b1f704c
GLinBoy <glinboy@live.com>
2024-07-17 18:45:20 +0200

    👌 IMPROVE: Changed the record to Class

--- src/main/java/com/glinboy/sample/reactive/service/dto/CommentDTO.java
+++ src/main/java/com/glinboy/sample/reactive/service/dto/CommentDTO.java
@@ -1,11 +1,16 @@
 package com.glinboy.sample.reactive.service.dto;
 
-public record CommentDTO(
-    String id,
-    String content,
-    String itemId,
-    String userId,
-    String parentId
-) {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentDTO {
+    private String id;
+    private String content;
+    private String itemId;
+    private String userId;
+    private String parentId;
 }
