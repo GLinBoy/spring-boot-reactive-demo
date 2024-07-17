6c15f92f58e981cbf0a6164a43fc03899130f464
GLinBoy <glinboy@live.com>
2024-07-17 18:22:39 +0200

    👌 IMPROVE: Added get all comment to REST API

--- src/main/java/com/glinboy/sample/reactive/web/rest/CommentResource.java
+++ src/main/java/com/glinboy/sample/reactive/web/rest/CommentResource.java
@@ -1,5 +1,22 @@
 package com.glinboy.sample.reactive.web.rest;
 
+import com.glinboy.sample.reactive.entity.Comment;
+import com.glinboy.sample.reactive.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/comments")
 public class CommentResource {
 
+    private final CommentService service;
+
+    @GetMapping
+    public Flux<Comment> getAllComments() {
+        return service.findAll();
+    }
 }
