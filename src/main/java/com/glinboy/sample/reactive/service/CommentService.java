faa801938d26aeb4209ba7813424e3ee00269f93
GLinBoy <glinboy@live.com>
2024-07-17 18:22:05 +0200

    👌 IMPROVE: Added get all comment to service API

--- src/main/java/com/glinboy/sample/reactive/service/CommentService.java
+++ src/main/java/com/glinboy/sample/reactive/service/CommentService.java
@@ -1,5 +1,10 @@
 package com.glinboy.sample.reactive.service;
 
+import com.glinboy.sample.reactive.entity.Comment;
+import reactor.core.publisher.Flux;
+
 public interface CommentService {
+
+    Flux<Comment> findAll();
 
 }
