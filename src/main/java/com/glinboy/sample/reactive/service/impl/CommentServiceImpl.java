04044a42ef97ad468940f64821766d07f2c68ef5
GLinBoy <glinboy@live.com>
2024-07-17 18:22:15 +0200

    👌 IMPROVE: Added get all comment to service impl

--- src/main/java/com/glinboy/sample/reactive/service/impl/CommentServiceImpl.java
+++ src/main/java/com/glinboy/sample/reactive/service/impl/CommentServiceImpl.java
@@ -1,7 +1,21 @@
 package com.glinboy.sample.reactive.service.impl;
 
+import com.glinboy.sample.reactive.entity.Comment;
+import com.glinboy.sample.reactive.repository.CommentRepository;
 import com.glinboy.sample.reactive.service.CommentService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 
+@Service
+@RequiredArgsConstructor
 public class CommentServiceImpl implements CommentService {
 
+    private final CommentRepository repository;
+
+    @Override
+    public Flux<Comment> findAll() {
+        return repository.findAll();
+    }
 }
